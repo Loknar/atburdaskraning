8dfc0356221b73477ccc2703ab5fb184dcb41788
Sveinn Flóki Guðmundsson <floki@tg-dw249.rhi.hi.is>
2013-11-18 16:21:04 +0000

    skrár sem þarf búnar til
    allt saman tómar skeljar, empty.php er tóma skelin ef við viljum gera
    fleiri 'tómar' php skrár

--- events.php
+++ events.php
@@ -23,82 +23,18 @@ require_once "php/head.php";
 
   <body>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Atburðaskráningasíða</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Forsíða</a></li>
-            <li><a href="events.php">Atburðir</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <a class="btn btn-default" href="login.php">
-            <span class="glyphicon glyphicon-user"></span> Innskráning
-            </a>
-          </form>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+<?php
+$index_active = " class=\"active\"";
+require_once "parts/navbar.php";
+?>
 
     <div class="container">
 
       <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">
-        Use this document as a way to quickly start any new project.<br> 
-        All you get is this text and a mostly barebones HTML document.
-        </p>
+        <h1>Tóm skel</h1>
         <p>
-<?php
-
-if (isset($_GET['title']))
-{
-	$title = $_GET['title'];
-	$insert = $db->prepare("INSERT INTO events (title) VALUES(:title);");
-	$insert->execute(array('title' => $title));
-	
-	echo "Bætti ".$title." við! <br />\n";
-}
-
-echo "Getting event titles from database.<br />\n";
-$result = $db->query("SELECT title FROM events;");
-foreach($result as $row_data)
-{
-	echo $row_data["title"]."\n";
-}
-
-?>
+        ...
         </p>
-      </div>
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Heading blah blah blah bla bla bla <small>dagsetning</small></h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
     </div><!-- /.container -->
 
 
