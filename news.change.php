a357b1c9c2590e4f24445353ffa658d573b1443a
Sveinn Flóki Guðmundsson <svefgud@gmail.com>
2013-11-24 22:45:38 +0000

    news.change.php
    moderator getur núna breytt skráðum fréttum, vantar að vísu að
    meðhöndla höfund og editor og tímasetningu postaðrar fréttar og
    breytinga

--- news.change.php
+++ news.change.php
@@ -1,6 +1,80 @@
 <?php
 require_once "php/head.php";
 
+// this page requires moderator or administrator login to access
+requireMod();
+
+// get id
+$news_id = intGET("id");
+
+// check if given id is valid or not, and fetch info about the event if id is valid
+// preparing statement
+$query = $db->prepare("SELECT title,description FROM news WHERE news_id = :news_id;");
+// insert variables safely into the prepared statement and execute it
+$query->execute(array('news_id' => $news_id));
+// fetch results into a results variable
+$news_result = $query->fetchAll();
+if (0 == count($news_result)) redirectToRoot(); // if no news with id then gtfo
+
+$row_data = $news_result[0];
+
+$title = $row_data["title"];
+$description = $row_data["description"];
+
+// =============
+// POST handling
+// =============
+
+// variables for the form
+$newsTitle       = $title;
+$date_created = "";
+$date_edited   = "";
+$newsDescription = $description;
+$newsCreator = "";
+$last_editor = "";
+
+$post_happened = false;
+
+if(isset($_POST["newsTitle"]) && 
+   isset($_POST["newsDescription"])) {
+  
+  $post_happened = true;
+  $post_success = true;
+  $post_error = "";
+  
+  $newsTitle       = get_post("newsTitle");
+  $newsDescription = get_post("newsDescription");
+  
+  // handle and check validity of the form variables
+  
+  // event title should not be empty string
+  if(strlen($newsTitle) > 0) {
+    // max length of event title is 200 characters
+    $newsTitle = truncateString($newsTitle, 200);
+  }
+  else {
+    $post_success = false;
+    $post_error .= "<li>Titill atburðar má ekki vera tómur strengur.</li>\n";
+  }
+  
+  // event description should not be empty string
+  if(strlen($newsDescription) == 0) {
+    $post_success = false;
+    $post_error .= "<li>Lýsing atburðar má ekki vera tómur strengur.</li>\n";
+  }
+  
+  if($post_success) {
+    // insert event into database
+    $insert = $db->prepare("UPDATE news SET title=:title,description=:description WHERE news_id=:news_id;");
+    $result = $insert->execute(array('title' => $newsTitle,'description' => $newsDescription,'news_id' => $news_id));
+    if(!$result) {
+      // $error = $insert->errorCode();
+      die("Database error."); // þarf kannski að meðhöndla eitthvað betur
+    }
+    // post succeeded
+  }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="is">
@@ -8,14 +82,15 @@ require_once "php/head.php";
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Vefforritunarverkefni 5 (TÖL306G haust 2013)">
+    <meta name="description" content="News Add">
     <meta name="author" content="Sveinn Flóki Guðmundsson">
     <link rel="shortcut icon" href="ico/favicon.ico">
 
     <title><?php echo $website_title; ?></title>
 
     <!-- Bootstrap core CSS -->
-    <link href="dist/css/bootstrap.css" rel="stylesheet">
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="dist/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 
     <!-- Custom styles -->
     <link href="css/main.css" rel="stylesheet">
@@ -24,17 +99,58 @@ require_once "php/head.php";
   <body>
 
 <?php
-$index_active = " class=\"active\"";
 require_once "parts/navbar.php";
 ?>
 
     <div class="container">
+      
+      <h1>Breyta skráðri frétt</h1>
+      
+<?php
 
-      <div class="starter-template">
-        <h1>Tóm skel</h1>
-        <p>
-        ...
-        </p>
+if($post_happened) {
+  if($post_success) {
+    echo <<<_END
+      <div class="alert alert-block alert-success fade in">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4>Frétt breytt</h4>
+        <p>Frétt hefur verið uppfærð.</p>
+      </div><!-- alert message -->
+_END;
+  }
+  else {
+    echo <<<_END
+      <div class="alert alert-block alert-danger fade in">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4>Úps! Eitthvað klikkaði</h4>
+        <p>Eftirfarandi villur komu upp:</p>
+        <ul>
+          $post_error
+        </ul>
+        <p>Lagaðu ofangreindar villur og reyndu svo aftur.</p>
+      </div><!-- alert message -->
+_END;
+  }
+}
+
+?>
+
+      <form role="form" method="post" action="news.change.php?id=<?php echo $news_id; ?>">
+        <div class="form-group">
+          <label for="newsTitle">Titill fréttar</label>
+          <input class="form-control" id="newsTitle" name="newsTitle" placeholder="Titill" value="<?php echo $newsTitle; ?>">
+        </div>
+
+        
+        <div class="form-group">
+          <label for="newsDescription">Lýsing</label>
+          <textarea id="newsDescription" name="newsDescription" class="form-control" rows="4" placeholder="Lýsing ..."><?php echo $newsDescription; ?></textarea>
+        </div>
+
+        
+        <button type="submit" class="btn btn-default">Breyta frétt</button>
+      </form>
+    
     </div><!-- /.container -->
 
 
@@ -43,5 +159,22 @@ require_once "parts/navbar.php";
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="assets/js/jquery.js"></script>
     <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/bootstrap-datetimepicker.min.js"></script>
+    <script type="text/javascript">
+      $(function () {
+        $('#datetimepicker1').datetimepicker({
+          pickSeconds: false
+        });
+        $('#datetimepicker2').datetimepicker({
+          pickSeconds: false
+        });
+        $('#datetimepicker3').datetimepicker({
+          pickSeconds: false
+        });
+        $('#datetimepicker4').datetimepicker({
+          pickSeconds: false
+        });
+      });
+    </script>
   </body>
 </html>
