9b5d499501a3cfebc58a06c8d09835609c0394c4
Sveinn Flóki Guðmundsson <floki@vr2t-dw246.rhi.hi.is>
2013-11-24 17:12:15 +0000

    skráningarlisti
    notandi getur nú séð skráningarlista og skráð sig og afskráð sig á
    skráningartíma

--- event.php
+++ event.php
@@ -1,6 +1,54 @@
 <?php
 require_once "php/head.php";
 
+// get id
+$event_id = intGET("id");
+
+// check if given id is valid or not, and fetch info about the event if id is valid
+
+// preparing statement
+$query = $db->prepare("SELECT title,start,end,registration_start,registration_end,description,location,seats FROM events WHERE event_id = :event_id;");
+// insert variables safely into the prepared statement and execute it
+$query->execute(array('event_id' => $event_id));
+// fetch results into a results variable
+$event_result = $query->fetchAll();
+
+if (0 < count($event_result)) $valid_event_id = True;
+else $valid_event_id = False;
+
+
+// get current time
+$now = time();
+
+// if id valid handle post register
+if($valid_event_id && isset($_POST["register"])) {
+  // preparing statement
+  $query = $db->prepare("SELECT user_id FROM registrations WHERE event_id=:event_id;");
+  // insert variables safely into the prepared statement and execute it
+  $query->execute(array('event_id' => $event_id));
+  // fetch results into a results variable
+  $result = $query->fetchAll();
+  
+  if (0 == count($result)) {
+    // preparing statement
+    $query = $db->prepare("INSERT INTO 'registrations' ('user_id','event_id','timing') VALUES (:user_id,:event_id,:timing);");
+    // insert variables safely into the prepared statement and execute it
+    $result = $query->execute(array('user_id' => USER_ID,'event_id' => $event_id, 'timing' => $now));
+
+    //echo "registered";
+  }
+}
+
+// if id valid handle post unregister
+if($valid_event_id && isset($_POST["unregister"])) {
+  // preparing statement
+  $query = $db->prepare("DELETE FROM 'registrations' WHERE user_id=:user_id AND event_id=:event_id;");
+  // insert variables safely into the prepared statement and execute it
+  $result = $query->execute(array('user_id' => USER_ID,'event_id' => $event_id));
+  
+  //echo "unregistering";
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="is">
@@ -31,10 +79,143 @@ require_once "parts/navbar.php";
     <div class="container">
 
       <div class="starter-template">
-        <h1>Tóm skel</h1>
+<?php
+
+if ($valid_event_id) {
+  // there should only be one result, let's fetch that
+  $row_data = $event_result[0];
+  
+  $title = $row_data["title"];
+  $start = intval($row_data["start"]);
+  $start_string = date("d-m-Y H:i",$start);
+  $end = intval($row_data["end"]);
+  $end_string = date("d-m-Y H:i",$end);
+  $registration_start = intval($row_data["registration_start"]);
+  $registration_start_string = date("d-m-Y H:i",$registration_start);
+  $registration_end = intval($row_data["registration_end"]);
+  $registration_end_string = date("d-m-Y H:i",$registration_end);
+  $description = nl2br($row_data["description"]);
+  $location = $row_data["location"];
+  $seats = intval($row_data["seats"]);
+  echo <<<_END
+        <h1>$title</h1>
+        <p class="text-muted">$start_string</p>
         <p>
-        ...
+        $description
         </p>
+        <h2>Skráning</h2>
+_END;
+  
+  // different things shown based on whether registartion is not yet started, started or over
+  if ($now < $registration_start) {
+    // registration not yet started
+    if (0 < $seats) {
+      $seats_string = "Sætafjöldi: $seats";
+    }
+    else {
+      $seats_string = "Engin fjöldatakmörkun.";
+    }
+    
+    echo <<<_END
+        <p>
+        Skráning hefst: $registration_start_string<br/>
+        Skráningu lýkur: $registration_end_string<br/>
+        $seats_string
+        </p>
+_END;
+  }
+  elseif ($registration_end < $now) {
+    // registration over
+    echo <<<_END
+        <p>Skráningu lokið.</p>
+_END;
+  }
+  else {
+    // registration open
+    echo <<<_END
+        <p>Takki til að skrá sig hér takk ... </p>
+        <form role="form" method="post" action="event.php?id=$event_id">
+          <input type="hidden" name="register" value="do it" />
+          <button type="submit" class="btn btn-success">Skrá mig</button>
+        </form>
+        
+        <p>... eða takki til að afskrá sig er maður er nú þegar skráður</p>
+        <!-- Button trigger modal -->
+        <button class="btn btn-danger" data-toggle="modal" data-target="#confirmUnregister">
+          Afskrá mig
+        </button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="confirmUnregister" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Staðfesta afskráningu</h4>
+              </div>
+              <div class="modal-body">
+                Þú ert við það að afskrá þig af viðkomandi atburði, þessa aðgerð er ekki hægt að draga til baka.
+                Ertu viss um að þú viljir afskrá þig?
+              </div>
+              <div class="modal-footer">
+                
+                <form role="form" method="post" action="event.php?id=$event_id">
+                  <input type="hidden" name="unregister" value="do it" />
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Hætta við</button>
+                  <button type="submit" class="btn btn-danger">Já, afskráðu mig</button>
+                </form>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+        
+_END;
+  }
+  // different things shown based on whether registartion is not yet started, started or over
+  
+  // now we want to fetch users that have registered to this event
+  
+  // preparing statement
+  $query = $db->prepare("SELECT name FROM users WHERE user_id = (SELECT user_id FROM registrations WHERE event_id=:event_id ORDER BY timing DESC);");
+  // insert variables safely into the prepared statement and execute it
+  $query->execute(array('event_id' => $event_id));
+  // fetch results into a results variable
+  $result = $query->fetchAll();
+  //var_dump($result);
+  
+  echo <<<_END
+        <ol>
+_END;
+  
+  $nr = 0;
+  foreach($result as $row_data) {
+    $name = $row_data["name"];
+    
+    $nr++;
+    $listyle = "";
+    if ($seats > 0 && $seats - $nr < 0) {
+      $listyle = " style=\"color: #BBB;\" ";
+    }
+    echo <<<_END
+        <li$listyle>$name</li>
+_END;
+  }
+  
+  echo <<<_END
+        </ol>
+_END;
+  
+}
+else {
+  echo <<<_END
+        <p>Atburður fannst ekki :(</p>
+
+_END;
+}
+
+?>
+
+      </div>
     </div><!-- /.container -->
 
 
