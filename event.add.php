a664ea146670a1d058d854f8727e2c30861c6f94
Sveinn Flóki Guðmundsson <svefgud@gmail.com>
2014-01-13 17:15:15 +0000

    böggur lagaður
    yfirsást að setja í gagnagrunninn hver skráði atburð og hvenær, lagað

--- event.add.php
+++ event.add.php
@@ -116,8 +116,9 @@ if(isset($_POST["eventTitle"]) &&
   
   if($post_success) {
     // insert event into database
-    $insert = $db->prepare("INSERT INTO 'events' ('title','start','end','registration_start','registration_end','description','location','seats') VALUES (:title,:start,:end,:registration_start,:registration_end,:description,:location,:seats)");
-    $result = $insert->execute(array('title' => $eventTitle,'start' => $eventStarting_Unixtime,'end' => $eventEnding_Unixtime,'registration_start' => $registerStarting_Unixtime,'registration_end' => $registerEnding_Unixtime,'description' => $eventDescription,'location' => $eventLocation,'seats' => $eventSeats_int));
+    $date_created = time();
+    $insert = $db->prepare("INSERT INTO 'events' ('title','start','end','registration_start','registration_end','description','location','seats','creator','date_created') VALUES (:title,:start,:end,:registration_start,:registration_end,:description,:location,:seats,:creator,:date_created)");
+    $result = $insert->execute(array('title' => $eventTitle,'start' => $eventStarting_Unixtime,'end' => $eventEnding_Unixtime,'registration_start' => $registerStarting_Unixtime,'registration_end' => $registerEnding_Unixtime,'description' => $eventDescription,'location' => $eventLocation,'seats' => $eventSeats_int,'creator' => USER_ID,'date_created' => $date_created));
     if(!$result) {
       // $error = $insert->errorCode();
       die("Database error."); // þarf kannski að meðhöndla eitthvað betur
